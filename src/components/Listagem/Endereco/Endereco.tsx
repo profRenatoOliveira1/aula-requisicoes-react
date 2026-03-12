0106080057f64dfa93f81f314dba13d58219a119
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-03-12 15:14:00 -0300

    feat
    Listando CEPs na página

--- src/components/Listagem/Endereco/Endereco.tsx
+++ src/components/Listagem/Endereco/Endereco.tsx
@@ -1,18 +1,68 @@
-import { type JSX } from "react";
+import { type JSX, useState } from "react";
 import './Endereco.css';
+import EnderecoRequests from "../../../fetch/EnderecoRequests";
+
+interface Endereco {
+    cep: string
+    state: string
+    city: string
+    neighborhood: string
+    street: string
+    location: {
+        type: string
+        coordinates: {
+            longitude: string
+            latitude: string
+        }
+    }
+}
 
 function Endereco(): JSX.Element {
+
+    const [cep, setCep] = useState("");
+    const [cepInfo, setCepInfo] = useState<Endereco | null>(null);
+
+    const fetchCEP = async () => {
+        try {
+            const retornoAPI = await EnderecoRequests.consultaCEP(cep);
+            setCepInfo(retornoAPI);
+        } catch (error) {
+            console.error(`Erro na busca. ${error}`);
+        }
+    }
+
     return (
         <section className="endereco">
             <div className="endereco-container">
                 <h1 className="endereco-cabecalho">Endereço</h1>
 
-                <input type="number" name="cep" id="cep" className="cep" onClick={() => {alert('Botão pressionado.')}} />
-                <button className="endereco-botao">Buscar endereço</button>
+                <input
+                    type="text"
+                    name="cep"
+                    id="cep"
+                    className="cep"
+                    onChange={(e) => setCep(e.target.value)}
+                />
 
-                {/* Implementar campos do resultado da busca */}
+                <button
+                    className="endereco-botao"
+                    onClick={fetchCEP}
+                >
+                    Buscar endereço
+                </button>
+
                 <section className="detalhes-endereco">
-                
+                    {cepInfo && (
+                        <>
+                            <p>Cidade: {cepInfo.city}</p>
+                            <p>Estado: {cepInfo.state}</p>
+                            <p>Rua: {cepInfo.street}</p>
+                            <p>Bairro: {cepInfo.neighborhood}</p>
+                            <p>Location: {cepInfo.location.type}</p>
+                            <p>Latitude: {cepInfo.location.coordinates.latitude}</p>
+                            <p>Longitude: {cepInfo.location.coordinates.longitude}</p>
+                        </>
+                    )}
                 </section>
             </div>
         </section>
--- src/fetch/EnderecoRequests.ts
+++ src/fetch/EnderecoRequests.ts
@@ -1,1 +1,23 @@
-// Classe para criar métodos que vão se comunicar com a API
+class EnderecoRequests {
+
+    private serverUrl: string;
+    private endpointCEP: string;
+
+    constructor() {
+        this.serverUrl = 'https://brasilapi.com.br'
+        this.endpointCEP = '/api/cep/v2/';
+    }
+
+    async consultaCEP(cep: string) {
+        try {
+            const response = await fetch(`${this.serverUrl}${this.endpointCEP}${cep}`);
+            const data = await response.json();
+
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export default new EnderecoRequests;
